10753a375be55c4daed7903f8b3bc17db85e4f44
Jason Gilbert <0jason.gilbert@gmail.com>
2022-09-20 21:31:26 -0300

    Trim excess code

--- Player.gd
+++ Player.gd
@@ -4,14 +4,6 @@ extends Node
 var soldier_count = 5
 var loot = 0
 
-# Icons.
-# I am putting this here until I create actual Units.
-var open_fire_icon = load("res://icons/OpenFireIcon.gd") # WAIT is this still being used?
-var take_cover_icon = load("res://icons/TakeCoverIcon.gd")
-
-# Strategies.
-# Same. Or not. Maybe it's not necessary.
-
 var icons = {Icons.open_fire: 2, Icons.take_cover: 1}
 var strategies = {Strategies.starter: 1}
 
